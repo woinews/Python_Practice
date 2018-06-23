a56ad5fd313cdb3677e006451a161ef752c46a13
woinews <inews101@163.com>
2018-06-24 00:33:10 +0800

    LM神经网络模型
    LM神经网络模型

--- Electric_leakage_detection/LM_neural_network.py
+++ Electric_leakage_detection/LM_neural_network.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_excel(r'..\electricity_model.xls')
+data = pd.read_excel(r'electricity_model.xls')
 data = data
 #划分训练数据和测试数据，20%作为训练数据，80%作为测试数据
 p = 0.8 #设置训练数据比例 
@@ -13,6 +13,9 @@ test = data.iloc[int(len(data)*p):,:]
 
 x = train.iloc[:,:3].as_matrix()
 y = train.iloc[:,3].as_matrix()
+
+x_test = test.iloc[:,:3].as_matrix()
+y_test = test.iloc[:,3].as_matrix()
 
 #构建LM神经网络模型
 from keras.models import Sequential #导入神经网络初始化函数
@@ -29,7 +32,7 @@ net.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics=['accuracy
 #使用metrics=['accuracy']来输出准确率
 
 net.fit(x, y, nb_epoch=1000, batch_size=50) #训练模型，循环1000次,batch_size每次训练和梯度更新块的大小为1（每次只训练一个样本）
-net.save_weights(r'..\net.model') #保存模型
+net.save_weights(r'net.model') #保存模型
 predictions = net.predict(x) #对数据进行预测，将训练模型运用于数据集x
 train['预测值'] = [int(np.round(x)) for x in predictions]
 
@@ -51,11 +54,16 @@ def cm_plot(y, yp):
 predict_result = net.predict_classes(x).reshape(len(y)) #预测结果变形,作为混淆矩阵函数的输入yp
 #这里要提醒的是，keras用predict给出预测概率，predict_classes才是给出预测类别，而且两者的预测结果都是n x 1维数组，而不是通常的 1 x n
 cm_plot(y, predict_result).show() #显示混淆矩阵可视化结果
+scores = net.evaluate(x, y)
+print("训练样本准确率：%s: %.2f%%" % (net.metrics_names[1], scores[1]*100)) #计算模型准确率
 
 from sklearn.metrics import roc_curve #导入ROC曲线函数
 
-predict_result = net.predict(test.iloc[:,:3]).reshape(len(test))
-fpr, tpr, thresholds = roc_curve(test.iloc[:,3], predict_result, pos_label=1)
+predict_result = net.predict(x_test).reshape(len(y_test))
+scores_test = net.evaluate(x_test, y_test)
+print("测试样本准确率：%s: %.2f%%" % (net.metrics_names[1], scores_test[1]*100)) #计算模型准确率
+
+fpr, tpr, thresholds = roc_curve(y_test, predict_result, pos_label=1)
 plt.plot(fpr, tpr, linewidth=2, label = 'ROC of LM') #作出ROC曲线
 plt.xlabel('False Positive Rate') #坐标轴标签
 plt.ylabel('True Positive Rate') #坐标轴标签
@@ -64,5 +72,3 @@ plt.xlim(0,1.05) #边界范围
 plt.legend(loc=4) #图例
 plt.show() #显示作图结果
 
-scores = net.evaluate(x, y)
-print("%s: %.2f%%" % (net.metrics_names[1], scores[1]*100)) #计算模型准确率
